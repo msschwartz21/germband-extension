09613666cfb6a6cba5da15e3e0c93af967a3c65a
Morgan Schwartz <msschwartz21@gmail.com>
2018-10-30 15:15:09 -0700

    Initial commit on new computer

--- notebooks/20181006.py
+++ notebooks/20181006.py
@@ -18,13 +18,55 @@
 #     version: 3.7.0
 # ---
 
-# %autosave 1
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-pwd
-
+from imp import reload
 import sys
 sys.path.insert(0, '..')
-import utilites as ut
+import utilities as ut
+
+from skimage import img_as_float
+
+hst = ut.read_hyperstack('../data/wt_gbe_20180110.h5')
+
+ut.imshow(hst[0,350:500,100:900])
+
+# # Segmentation to remove background
+
+fhst = img_as_float(hst)
+
+test = fhst[0,350:500,100:900]
+
+ut.imshow(test)
+
+from scipy.ndimage import gaussian_filter
+
+gaus = gaussian_filter(test,4)
+ut.imshow(gaus)
+
+
+
+from skimage.feature import canny
+
+edges = canny(gaus)
+type(edges)
+
+plt.imshow(edges)
+
+from scipy import ndimage as ndi
+fill = ndi.binary_fill_holes(edges)
+plt.imshow(fill)
+
+from skimage.filters import sobel
+elevation_map = sobel(test)
+ut.imshow(elevation_map)
+
+markers = np.zeros_like(test)
+markers[test<0.1] = 1
+markers[test>0.9] = 2
+plt.imshow(markers)
+
+from skimage.morphology import watershed
+
+segmentation = watershed(elevation_map,markers)
+plt.imshow(segmentation)
